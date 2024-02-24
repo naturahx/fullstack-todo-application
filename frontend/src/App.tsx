b179261d267c03933104fec17b3b5fa254d7d84f
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-24 22:39:57 +0300

    validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import React, { FC, useEffect, useState } from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import "./App.css";
 
 interface AppItem {
@@ -6,56 +8,75 @@ interface AppItem {
   description: string;
 }
 
-const App: FC = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [data, setData] = useState([]);
+const validationSchema = Yup.object().shape({
+  title: Yup.string().min(3, 'Название должно содержать не менее 3 символов').required('Название обязательно'),
+  description: Yup.string().min(5, 'Описание должно содержать не менее 5 символов').required('Описание обязательно'),
+});
 
+const App: FC = () => {
+  const [data, setData] = useState<AppItem[]>([]);
+  
   useEffect(() => {
     fetch("http://localhost:3000")
       .then((response) => response.json())
       .then((data) => setData(data));
-  }, [title]);
+  }, [data]);
 
-  const handleButtonClick = () => {
-    const data = { title: title, description: description };
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values: any) => {
+      const newData = { title: values.title, description: values.description };
 
-    fetch("http://localhost:3000", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
-    setDescription("");
-    setTitle("");
-  };
+      fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setData([...data, newData]);
+        })
+        .catch((error) => console.error("Error:", error));
+
+      formik.resetForm();
+    },
+  });
 
   return (
     <>
       <h1>FullStack ToDo</h1>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <input
+          name="title"
           placeholder="Название"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={formik.values.title}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+
         <input
+          name="description"
           placeholder="Описание"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          value={formik.values.description}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
-        <button onClick={handleButtonClick}>Добавить</button>
+
+        <button type="submit">Добавить</button>
       </form>
 
       <div className="block">
         <div>
           <ul>
-            {data.map((item: AppItem) => (
-              <li>
+            {data.map((item: AppItem, index) => (
+              <li key={index}>
                 <h3>Название: {item.title}</h3>
                 <p>Описание: {item.description}</p>
                 <button>Удалить</button>
