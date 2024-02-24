438963cf497c812f0cdaa97b4294bc699becf6ba
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-24 20:07:05 +0300

    styles + small fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,70 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from "react";
+import "./App.css";
 
 interface AppItem {
-  title: string,
-  description: string
+  title: string;
+  description: string;
 }
 
-const App:FC = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [data, setData] = useState([])
+const App: FC = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000')
-    .then(response => response.json())
-    .then(data => setData(data))
-  }, [title])
+    fetch("http://localhost:3000")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, [title]);
 
   const handleButtonClick = () => {
     const data = { title: title, description: description };
 
-    fetch('http://localhost:3000', {
-      method: 'POST',
+    fetch("http://localhost:3000", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-      setDescription('')
-      setTitle('')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+    setDescription("");
+    setTitle("");
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button onClick={handleButtonClick}>Отправить на сервер</button>
+    <>
+      <h1>FullStack ToDo</h1>
+      <form>
+        <input
+          placeholder="Название"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          placeholder="Описание"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={handleButtonClick}>Добавить</button>
+      </form>
 
-      <ul>
-        {data.map((item: AppItem) => (
-          <li>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="block">
+        <div>
+          <ul>
+            {data.map((item: AppItem) => (
+              <li>
+                <h3>Название: {item.title}</h3>
+                <p>Описание: {item.description}</p>
+                <button>Удалить</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
