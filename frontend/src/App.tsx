785fa449414b4f750741019df5d27d7fd9e2c2e6
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-25 00:01:16 +0300

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import "./App.css";
 
 interface AppItem {
@@ -8,63 +7,55 @@ interface AppItem {
 }
 
 const App: FC = () => {
-  const [data, setData] = useState<AppItem[]>([]);
-  const { register, handleSubmit } = useForm<AppItem>(); // Инициализация react-hook-form
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = () => {
     fetch("http://localhost:3000")
       .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  };
+      .then((data) => setData(data));
+  }, [title]);
 
-  const addPost = (newPost: AppItem) => {
+  const handleButtonClick = () => {
+    const data = { title: title, description: description };
+
     fetch("http://localhost:3000", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newPost),
+      body: JSON.stringify(data),
     })
       .then((response) => response.json())
-      .then(() => {
-        fetchData(); // Обновление данных с сервера после успешного добавления поста
-      })
-      .catch((error) => console.error("Error adding post:", error));
-  };
-
-  const onSubmit = (data: AppItem) => {
-    addPost(data);
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+    setDescription("");
+    setTitle("");
   };
 
   return (
     <>
       <h1>FullStack ToDo</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form>
         <input
-          name="title"
           placeholder="Название"
-          ref={register({ required: 'Название обязательно', minLength: { value: 3, message: 'Название должно содержать не менее 3 символов' } }) as any}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-
         <input
-          name="description"
           placeholder="Описание"
-          ref={register({ required: 'Описание обязательно', minLength: { value: 5, message: 'Описание должно содержать не менее 5 символов' } }) as any}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
-
-        <button type="submit">Добавить</button>
+        <button onClick={handleButtonClick}>Добавить</button>
       </form>
 
       <div className="block">
         <div>
           <ul>
-            {data.map((item: AppItem, index) => (
-              <li key={index}>
+            {data.map((item: AppItem) => (
+              <li>
                 <h3>Название: {item.title}</h3>
                 <p>Описание: {item.description}</p>
                 <button>Удалить</button>
