04a59e992d6f2defce2247b255207dcd4a01cdda
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-25 15:16:20 +0300

    full

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import "./App.css";
 
 interface AppItem {
@@ -7,9 +8,8 @@ interface AppItem {
 }
 
 const App: FC = () => {
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
   const [data, setData] = useState<AppItem[]>([]);
+  const { register, handleSubmit, formState: { errors } } = useForm<AppItem>();
 
   useEffect(() => {
     fetchData();
@@ -22,53 +22,54 @@ const App: FC = () => {
       .catch((error) => console.error("Error fetching data:", error));
   };
 
-  const addPost = (title: string, description: string) => {
+  const addPost = (newPost: AppItem) => {
     fetch("http://localhost:3000", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ title, description }),
+      body: JSON.stringify(newPost),
     })
       .then((response) => response.json())
       .then(() => {
         fetchData(); // Обновление данных с сервера после успешного добавления поста
       })
       .catch((error) => console.error("Error adding post:", error));
+  };
 
-    setDescription("");
-    setTitle("");
+  const onSubmit = (formData: AppItem) => {
+    addPost(formData);
   };
 
   return (
     <>
       <h1>FullStack ToDo</h1>
-      <form onSubmit={(e) => { e.preventDefault(); addPost(title, description); }}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           placeholder="Название"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          {...register("title", { required: 'Название обязательно', minLength: { value: 3, message: 'Название должно содержать не менее 3 символов' } })}
         />
+        {errors.title && <p>{errors.title.message}</p>}
+
         <input
           placeholder="Описание"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          {...register("description", { required: 'Описание обязательно', minLength: { value: 5, message: 'Описание должно содержать не менее 5 символов' } })}
         />
+        {errors.description && <p>{errors.description.message}</p>}
+
         <button type="submit">Добавить</button>
       </form>
 
       <div className="block">
-        <div>
-          <ul>
-            {data.map((item: AppItem, index) => (
-              <li key={index}>
-                <h3>Название: {item.title}</h3>
-                <p>Описание: {item.description}</p>
-                <button>Удалить</button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul>
+          {data.map((item: AppItem, index) => (
+            <li key={index}>
+              <h3>Название: {item.title}</h3>
+              <p>Описание: {item.description}</p>
+              <button>Удалить</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
